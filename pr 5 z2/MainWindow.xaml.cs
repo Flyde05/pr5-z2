636a65784c8c3442e0f3f93a9274ffc48e04ec92
Timur <zhilin.timur06@mail.ru>
2023-11-30 10:58:48 +0300

    Добавьте файлы проекта.

---
+++ pr 5 z2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr_5_z2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(textBoxA.Text);
+            int b = Convert.ToInt32(textBoxB.Text);
+            int count = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    count++;
+                }
+            }
+
+            resultTextBlock.Text = $"Количество нечетных чисел на отрезке [{a},{b}]: {count}";
+        }
+    }
+}
+    
+ 
